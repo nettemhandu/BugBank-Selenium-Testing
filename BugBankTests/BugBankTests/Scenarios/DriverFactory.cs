9d54bef3cddda2ac8cc66c53ce7d2b2a55b937e1
nettemhandu <antonetteliebe@icloud.com>
2025-12-20 19:37:04 +0200

    added DriverFactory class for setUp and TearDown

---
+++ BugBankTests/BugBankTests/Scenarios/DriverFactory.cs
@@ -0,0 +1,25 @@
+using System;
+using Gauge.CSharp.Lib.Attrribute;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Steps
+{
+    public class DriverFactory
+    {
+        public static IWebDriver Driver {get; private set;}
+
+        [BeforeSuite]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+        }
+
+        [AfterSuite]
+        public void TearDown()
+        {
+            Driver.Close();
+        }
+    }
+}
