5a70891d8fc6c25ffeaf72103dfb883095992041
nettemhandu <antonetteliebe@icloud.com>
2025-12-20 21:18:57 +0200

    added login pages locators

--- BugBankTests/BugBankTests/Scenarios/page/login.cs
+++ BugBankTests/BugBankTests/Scenarios/page/login.cs
@@ -12,8 +12,19 @@ namespace Pages
         private static IWebElement EnterEmail => Driver.FindElement(By.Name("email"));
         private static IWebElement EnterPassword => Driver.FindElement(By.Name("password"));
 
-        private static IWebElement
+        private static IWebElement AccessButton => Driver.FindElement(By.Text("Access"));
 
         private static IWebElement RegisterButton => Driver.FindElement(By.Text("Register"));
+
+        private static IWebElement EnterName => Driver.FindElement(By.Name("name"));
+
+        private static IWebElement ConfirmPassword => Driver.FindElement(By.Name("passwordConfirmation"));
+
+        private static IWebElement CreateAccWithBalance => Driver.FindElement(By.Id("toggleAddBalance"));
+
+        public LoginPage Open()
+        {
+            Driver.Navigate
+        }
     }
 }
